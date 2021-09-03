05206ab2f40da4f12d885aeb5bee8ac351bc3184
milenasvieira <milenasvieira63@gmail.com>
2021-09-03 11:37:50 -0300

    adding .gitignore

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+//const routes = require('routes.js')
 const app = express()
 const port = 3000
+
+app.use(express.json())
+//routes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
