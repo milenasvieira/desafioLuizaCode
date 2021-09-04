2de6186036ea3bd3ae28a3988b5d68718499cfa9
milenasvieira <milenasvieira63@gmail.com>
2021-09-03 21:25:50 -0300

    atualizacao

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
-//const routes = require('routes.js')
 const app = express()
 const port = 3000
 
 app.use(express.json())
-//routes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
