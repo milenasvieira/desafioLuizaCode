b99999e8aa2fbd484a11bfec88518950a36cfd01
Jade Ohara <jade-ohara@hotmail.com>
2021-09-12 13:23:40 -0300

    orderController funcionando

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,63 +1,70 @@
 const ShoppingCart = require('../models/ShoppingCart');
 const Order = require('../models/Order')
-const OrderProduct = require('../models/OrderProduct');
-const CartProduct = require('../models/CartProduct');
+var Sequelize = require('sequelize');
+var dbConfig = require('../services/database')
+
+const sequelize = new Sequelize(dbConfig);
 
 module.exports = {
     async create(req, res) {
 
         const { shoppingCartId, storeId } = req.body;
-        //const { shoppingCartId } = req.params;
 
-        // clientId, storeId, orderStatus
         try {
-            console.log("shoppingCartId", shoppingCartId)
             const shoppingCartItens = await ShoppingCart.findByPk(shoppingCartId)
-            console.log("shoppingCartItens", shoppingCartItens)
 
             if (shoppingCartItens == null) throw ("Não foi possível finalizar a compra. O carrinho não existe.")
-            //verificar se criou  duas vezes a mesma compra
 
             const shoppingCartFindOneInOrder = await Order.findOne({
                 where: { shoppingCartId }
             });
-            console.log("shoppingCartFindOneInOrder", shoppingCartFindOneInOrder)
+
             if (shoppingCartFindOneInOrder !== null) throw ("Esta compra já foi realizada.")
 
             const orderItens = {
                 shoppingCartId: shoppingCartId,
                 clientId: shoppingCartItens.clientId,
-                storeId: storeId,   //verificar nulo
-                orderStatus: 1    //1 - Compre realizada
+                storeId: storeId,  
+                orderStatus: 1    //1 - Compra realizada    //2 - Compra retirada
             }
 
             const orderCreate = await Order.create(orderItens)
-            console.log("orderCreate", orderCreate)
 
             const orderID = orderCreate.id
 
+            //USAR AQUI ATUALIZAR  ORDERPRODUCTS
+            sequelize.query(`
+            INSERT INTO orderproducts (orderId, productId, name, value)
+            SELECT orders.id, cartproducts.productId, cartproducts.name, cartproducts.value 
+            FROM cartproducts
+            INNER JOIN orders
+            ON cartproducts.shoppingCartId = ${shoppingCartId} = orders.shoppingCartId = ${shoppingCartId};`, { 
+                type:Sequelize.QueryTypes.INSERT
+            }) // .then(function(results) {
+            //     console.log(results) // or do whatever you want
+            // })
 
-            //fazer um loop pra criar todos  os  registros de cart  products em order  products
-            const cartProductsItens = await CartProduct.findAll({
-                where: { shoppingCartId }
-            })
-            console.log("cartProductsItens", cartProductsItens)
+            // DELETAR DAQUI
+            // const cartProductsItens = await CartProduct.findAll({
+            //     where: { shoppingCartId }
+            // })
+            // console.log("cartProductsItens", cartProductsItens)
 
-            const cartProductKeys = cartProductsItens.keys(dataValues)
-            console.log("cartProductKeys",cartProductKeys)
+            // const cartProductKeys = cartProductsItens.keys(dataValues)
+            // console.log("cartProductKeys",cartProductKeys)
 
-            const orderProductItens = {
-                orderId: orderID,
-                productId: cartProductsItens.productId,
-                name: cartProductsItens.name,
-                value: cartProductsItens.value
-            }
+            // const orderProductItens = {
+            //     orderId: orderID,
+            //     productId: cartProductsItens.productId,
+            //     name: cartProductsItens.name,
+            //     value: cartProductsItens.value
+            // }
             
+            // const orderProductCreate = await OrderProduct.create(orderProductItens)
+            // console.log("orderProductCreate", orderProductCreate)
+            // ATÉ AQUI
 
-            const orderProductCreate = await OrderProduct.create(orderProductItens)
-            console.log("orderProductCreate", orderProductCreate)
-
-            await ShoppingCart.update({
+            await ShoppingCart.update({ //verificar se atualizou a tabela
                 isFinished: true
             }, {
                 where: {
